6edde95d2704211749a8bda4fb23247f96a0428a
Suzic <suzichenapk@outlook.com>
2017-10-05 17:53:54 +0800

    第二部分完成 实现了列表样式及其基本逻辑

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,98 @@ export class Hero {
   id: number;
   name: string;
 }
-
+// 模拟数据
+const HEROES: Hero[] = [
+  { id: 11, name: "张三" },
+  { id: 12, name: "李四" },
+  { id: 13, name: "王五" },
+  { id: 14, name: "赵六" },
+  { id: 15, name: "小明" },
+  { id: 16, name: "小红" },
+  { id: 17, name: "小李" },
+  { id: 18, name: "小王" },
+  { id: 19, name: "小张" },
+  { id: 20, name: "苏子晨" },
+]
+// 组件部分
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} 详情</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="输入名字实现双向绑定">
+    <h2>我的英雄：</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" 
+        (click)="onSelect(hero)"
+        [class.selected]="hero === selectedHero">
+        <span class="badge">{{hero.id}}</span>{{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} 英雄详情</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+          <label>name: </label>
+          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>
   `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
-
+// 业务逻辑
 export class AppComponent  { 
   title = '英雄列表:';
-  hero: Hero = {
-    id: 1,
-    name: '苏子晨'
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero; //将点击的hero赋值给selectedHero
   }
 }
 

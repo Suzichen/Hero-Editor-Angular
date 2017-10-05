a0087cffadf2d904d5563803e153db394010ab76
Suzic <suzichenapk@outlook.com>
2017-10-05 16:54:34 +0800

    第一部分完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
+// 创建Hero对象
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>你好 {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} 详情</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="输入名字实现双向绑定">
+    </div>
+  `,
 })
-export class AppComponent  { name = '英雄'; }
+
+export class AppComponent  { 
+  title = '英雄列表:';
+  hero: Hero = {
+    id: 1,
+    name: '苏子晨'
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// 导入formsModule模块
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
-
+// 当前应用正在使用的外部模块列表↓
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
